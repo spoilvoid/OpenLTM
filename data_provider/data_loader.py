b3553895939dad4189f7e41bcb211d6ce67006c8
Zengyu-Ye <yzy1651717534@gmail.com>
2025-05-14 09:54:49 +0800

    TM_Models initialization

--- data_provider/data_loader.py
+++ data_provider/data_loader.py
@@ -368,9 +368,11 @@ class Dataset_ERA5_Pretrain(Dataset):
         num_train = int(len(df_raw) * 0.7)
         num_test = int(len(df_raw) * 0.2)
         num_vali = len(df_raw) - num_train - num_test
+        # start index
         border1s = [0, num_train - self.seq_len,
                     len(df_raw) - num_test - self.seq_len]
-        border2s = [num_train, num_train + num_vali, len(df_raw)]
+        # end index
+        border2s = [num_train, num_train + num_vali, len(df_raw)]       
         border1 = border1s[self.set_type]
         border2 = border2s[self.set_type]
 
---
+++ scripts/adaptation/full_shot/timer_xl_from_pth.sh
@@ -0,0 +1,31 @@
+export CUDA_VISIBLE_DEVICES=0
+model_name=timer
+token_num=30
+token_len=96
+seq_len=$[$token_num*$token_len]
+
+python -u run.py \
+  --task_name forecast \
+  --is_training 1 \
+  --root_path ./dataset/D1/ETT-small/ \
+  --data_path ETTh1.csv \
+  --model_id ETTh1_full_shot \
+  --model $model_name \
+  --data UnivariateDatasetBenchmark  \
+  --seq_len $seq_len \
+  --input_token_len $token_len \
+  --output_token_len $token_len \
+  --test_seq_len $seq_len \
+  --test_pred_len 96 \
+  --e_layers 8 \
+  --d_model 1024 \
+  --d_ff 2048 \
+  --batch_size 2048 \
+  --learning_rate 5e-6 \
+  --train_epochs 10 \
+  --gpu 0 \
+  --cosine \
+  --tmax 10 \
+  --use_norm \
+  --adaptation \
+  --pretrain_model_path checkpoints/timer_base_84m.pth
